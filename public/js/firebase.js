a16d399e15df99786209ac134ee355fe4535ff05
Geraldine <grcentinaje@gmail.com>
2019-04-11 22:35:23 -0700

    added firebase

---
+++ public/js/firebase.js
@@ -0,0 +1,10 @@
+// INITIALIZING FIREBASE
+var config = {
+  apiKey: "AIzaSyBveIilDp2TcLtpTUxsiu3NLyS9747yLYI",
+  authDomain: "project-2-ef3f9.firebaseapp.com",
+  databaseURL: "https://project-2-ef3f9.firebaseio.com",
+  projectId: "project-2-ef3f9",
+  storageBucket: "project-2-ef3f9.appspot.com",
+  messagingSenderId: "855350054422"
+};
+firebase.initializeApp(config);
---
+++ public/js/indexHandlers.js
@@ -0,0 +1,100 @@
+$(document).ready(function () {
+
+    $("#signUpContainer").hide();
+  
+    var database = firebase.database();
+  
+    // LOG IN /////////////////////////
+    $("#signInBtn").on("click", function (event) {
+  
+      event.preventDefault();
+  
+      var email = $("#emailSignIn").val();
+      var password = $("#passwordSignIn").val();
+  
+      firebase.auth().signInWithEmailAndPassword(email, password)
+  
+        .then(function (user) {
+  
+          var url = "../public/homepage.html";
+          $(location).attr('href', url);
+          console.log(user);
+  
+        })
+        .catch(function (error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+  
+          if (errorCode == 'auth/weak-password') {
+            bootbox.alert('The password does not contain at least six characters.');
+          } else {
+            bootbox.alert("User does not exist. Please sign up.");
+          }
+  
+        });
+    });
+  
+  
+  
+    // SIGN UP //////////////////////
+  
+    $("#signUpLink").on("click", function (event) {
+  
+      event.preventDefault();
+  
+      $("#signInContainer").hide();
+      $("#signUpContainer").show();
+  
+    });
+  
+    $("#signUpBtn").on("click", function (event) {
+  
+      event.preventDefault();
+  
+      var email = $("#emailSignUp").val();
+      var password = $("#passwordSignUp").val();
+  
+      console.log(email);
+  
+      firebase.auth().createUserWithEmailAndPassword(email, password)
+  
+        .then(function (user) {
+  
+          var url = "homepage.html";
+          $(location).attr('href', url);
+  
+        })
+  
+        .catch(function (error) {
+          console.log(error);
+          // Handle Errors here.
+          var errorCode = error.code;
+          // var errorMessage = error.message;
+          if (errorCode == 'auth/weak-password') {
+            alert('The password does not contain at least six characters.');
+  
+          }
+  
+        })
+
+        console.log(firebase.auth().currentUser);
+
+    });
+
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+          // User logged in already or has just logged in.
+          console.log(user.uid);
+        } else {
+          // User not logged in or has just logged out.
+        }
+      });
+
+    firebase.auth().currentUser
+  
+    
+  
+  });
+  
+  
